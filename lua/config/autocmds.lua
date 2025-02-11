44d60cfa329fc512ba080bf2772c9be62d50b696
EliBrank <eliasbrinkman@gmail.com>
2025-02-11 13:48:44 -0600

    change tab settings from options to html-specific autocommand

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html",
+  callback = function()
+    local num_tabs = 2
+    vim.opt_local.tabstop = num_tabs
+    vim.opt_local.shiftwidth = num_tabs
+    vim.opt_local.expandtab = true
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
-vim.opt.expandtab = true -- Pressing the TAB key will insert actual tab characters
-vim.opt.tabstop = 2 -- Number of spaces tabs count for
 vim.g.autoformat = false -- Globally prevent autoformat on save
-vim.opt.shiftwidth = 2 -- Number of spaces for auto-indentation
+
+-- local num_tabs = 2
+-- vim.opt.expandtab = true -- Pressing the TAB key will insert actual tab characters
+-- vim.opt.tabstop = num_tabs -- Number of spaces tabs count for
+-- vim.opt.shiftwidth = num_tabs -- Number of spaces for auto-indentation
 
 vim.opt.viminfo:remove('!')
