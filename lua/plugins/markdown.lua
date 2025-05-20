713499d23a76ab509c352fad8e84e0eacc1ee7fe
eliBrank <ezb.business@pm.me>
2025-05-20 13:53:07 -0500

    add tailwind configuration

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,10 +2,12 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
     init = function()
       vim.g.mkdp_browser = ''  -- Use default browser
       vim.g.mkdp_theme = 'dark'  -- or 'light'
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
   },
 }
