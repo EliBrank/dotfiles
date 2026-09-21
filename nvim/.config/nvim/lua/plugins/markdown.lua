05754166891762b46e53d576c4c28ac28ffb702a
EliBrank <eliasbrinkman@gmail.com>
2026-09-21 08:25:29 -0500

    change render modes for markdown renderer

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -5,7 +5,9 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    render_modes = { 'n', 'c', 't' }
+  },
 
   config = function(_, opts)
     vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { bg = "#504945" })
--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -17,5 +17,37 @@ return {
     checkbox = {
       order = { " ", "x" },
     },
-  }
+  },
+  -- keys = {
+  --   {
+  --     "<leader>ob",
+  --     "<cmd>Obsidian backlinks<cr>",
+  --     desc = "Backlinks",
+  --     ft = "markdown",
+  --   },
+  --   {
+  --     "<leader>of",
+  --     "<cmd>ObsidianQuickSwitch<cr>",
+  --     desc = "Quick Switch",
+  --     ft = "markdown",
+  --   },
+  --   {
+  --     "<leader>on",
+  --     "<cmd>ObsidianNew<cr>",
+  --     desc = "New Note",
+  --     ft = "markdown",
+  --   },
+  --   {
+  --     "<leader>or",
+  --     "<cmd>ObsidianRename<cr>",
+  --     desc = "Rename Note",
+  --     ft = "markdown",
+  --   },
+  --   {
+  --     "<leader>ot",
+  --     "<cmd>ObsidianTags<cr>",
+  --     desc = "Tags",
+  --     ft = "markdown",
+  --   },
+  -- },
 }
